df37954e9b294a4f63b6cf30b92e921bc5c3616c
impact30 <ernestopizarro3007@gmail.com>
2025-04-29 18:29:21 -0500

    Actualizado

---
+++ Locomotora.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Locomotora
+    {
+        Vagon Primero { get; set; }
+        Vagon Ultimo { get; set; }
+
+        public void AgregaPrimero(int valor)
+        {
+            Vagon nuevo = new Vagon(valor);
+            if (this.Primero == null)//caso 1
+            {
+                this.Primero = nuevo;
+            }
+            else //caso 2
+            {
+                Vagon primVag = this.Primero;
+
+                nuevo.Sig = primVag;
+
+                this.Primero = nuevo;
+
+            }
+
+        }
+
+
+        public void AgregaFinal(int valor)
+        {
+            Vagon nuevo = new Vagon(valor);
+            if (this.Primero == null)//caso 1
+            {
+                this.Primero = nuevo;
+            }
+            else //caso 2
+            {
+                Vagon tmp = this.Primero;
+                while (tmp.Sig != null)
+                {
+                    tmp = tmp.Sig;
+                }
+                tmp.Sig = nuevo;
+            }
+        }
+        public string VerVagones()
+        {
+            string listaVag = "";
+            Vagon tmp = this.Primero;
+            while (tmp != null)
+            {
+                listaVag += tmp.Dato + ", ";
+                tmp = tmp.Sig;
+            }
+            return listaVag;
+        }
+
+        public void EliminarUltimo()
+
+        {
+            if (this.Primero == null) //CUANDO SOLO ESTA LA LOCOMOTORA
+
+            {
+                Console.WriteLine("No hay vagones para eliminar.");
+                return;
+            }
+            if (this.Primero.Sig == null) // CUANDO HAY UN VAGON 
+
+            {
+
+                this.Primero = null;
+
+                return;
+
+            }
+            Vagon tmp = this.Primero;
+
+            while (tmp.Sig != null && tmp.Sig.Sig != null) //CUANDO HAY 2 O MAS VAGONES
+
+            {
+
+                tmp = tmp.Sig;
+
+            }
+            tmp.Sig = null;
+        }
+        public bool ExisteValor(int valor)
+        {
+            string listaVag = "";
+            Vagon tmp = this.Primero;
+            while (tmp != null)
+            {
+                if (tmp.Dato == valor)
+                {
+                    return true;
+                }
+                tmp = tmp.Sig;
+            }
+            return false;
+        }
+
+        public int GetValor(int pos)
+        {
+            return 0;
+        }
+        public void AgregaLEFinal(Locomotora otro)
+        {
+            Vagon tmpOtro = otro.Primero;
+
+            while (tmpOtro != null)
+            {
+                this.AgregaFinal(tmpOtro.Dato);
+                tmpOtro = tmpOtro.Sig;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿
+using System.Timers;
+using ConsoleApp3;
+
+Locomotora l = new Locomotora();
+l.AgregaFinal(2);
+l.AgregaFinal(10);
+l.AgregaFinal(20);
+l.AgregaFinal(40);
+l.AgregaFinal(30);
+
+Locomotora l2 = new Locomotora();
+l2.AgregaFinal(99);
+l2.AgregaFinal(100);
+l2.AgregaFinal(200);
+l.AgregaLEFinal(l2);
+
+Console.WriteLine(l.VerVagones());
+
+if (l.ExisteValor(50) == true)
+{
+    Console.WriteLine("El valor si existe en la lista");
+}
+else
+{
+    Console.WriteLine("El valor no existe en la lista");
+}
